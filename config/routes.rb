7c34878b905948db1289a5cb1dccd8823f63a481
Justin Wang <jswang.us@gmail.com>
2015-08-10 17:03:35 -0700

    added root controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root ''
+  root to: 'root#root'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
