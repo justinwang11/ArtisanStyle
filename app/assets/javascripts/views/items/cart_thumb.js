be7de7bf252fc12136501ed8e1e1ee1ccfbb1e57
Justin Wang <jswang.us@gmail.com>
2015-08-15 19:01:48 -0700

    added purchase and remove buttons to cart

--- app/assets/javascripts/views/items/cart_thumb.js
+++ app/assets/javascripts/views/items/cart_thumb.js
@@ -2,15 +2,35 @@ ArtisanStyle.Views.CartThumb = Backbone.View.extend({
 
   template: JST['items/cart_thumb'],
 
+  events: {
+    "click .checkoutbtn": "checkout",
+    "click .removeFromCart": "removeItem"
+  },
+
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
   },
 
   render: function () {
     var content = this.template({ item: this.model });
-    debugger;
     this.$el.html(content);
     return this;
+  },
+
+  checkout: function (event) {
+    event.preventDefault();
+    var item_id = this.model.get('id');
+    var cookies = Cookies.getJSON('ArtisanStyleCart');
+    delete cookies[item_id];
+    Cookies.set('ArtisanStyleCart', cookies);
+  },
+
+  removeItem: function (event) {
+    event.preventDefault();
+    var item_id = this.model.get('id');
+    var cookies = Cookies.getJSON('ArtisanStyleCart');
+    delete cookies[item_id];
+    Cookies.set('ArtisanStyleCart', cookies);
   }
 
 });
