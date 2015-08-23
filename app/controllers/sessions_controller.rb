e4869c711e9b44b5f962338a6a9ae06ff4c2a65c
Justin Wang <jswang.us@gmail.com>
2015-08-22 22:04:38 -0700

    transitions, error handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       sign_in(user)
       redirect_to root_url
     else
-      flash.now[:errors] = ["Invalid username or password"]
+      flash.now[:errors] = "Invalid username or password"
       render :new
     end
   end
